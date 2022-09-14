380b2682dd13e53bf2759ba063c2e22a6fe26836
yan <yanccomp@gmail.com>
2022-09-14 19:23:17 -0300

    Files

---
+++ listaex01/ex1/numeroComplexo.py
@@ -0,0 +1,8 @@
+def NumeroComplexo():
+
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    
+    def soma(self,numComplex):
+        return NumeroComplexo(self.a+numComplex.a)    
