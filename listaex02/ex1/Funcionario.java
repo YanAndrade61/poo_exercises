d9e11d243ab26c9c76ed7807c57c1be8e9f1b50e
yan <yanccomp@gmail.com>
2022-09-20 00:14:28 -0300

    ex2 Classe funcionario

---
+++ listaex02/ex1/Funcionario.java
@@ -0,0 +1,32 @@
+package listaex02.ex1;
+
+public class Funcionario {
+    
+    private String nome;
+    private String email;
+
+    public Funcionario() {
+        this.nome = "";
+        this.email = "";
+    }
+    public Funcionario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void exibeDados(){
+        System.out.printf("/nNome: %s /nEmail: %s",this.nome,this.email);
+    }
+
+}
